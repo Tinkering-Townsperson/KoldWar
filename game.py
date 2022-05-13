4f4b076c6bc0459592b38a56021d72b5097b1868
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-05-13 00:06:58 -0700

    Update game.py

--- game.py
+++ game.py
@@ -8,9 +8,7 @@ import os
 bunker = adv.Room(
 	"""
 You are in the nuclear bunker. The walls are made of drab white concrete.
-Hanging on the east wall are flags of the communist states of the world, 
-including The Republic of Cuba, The Federal Republic of East Germany,
-and The People's Republic of China, forever comrades to The USSR.
+Hanging on the east wall are flags of the communist states of the world, forever comrades to The USSR.
 On the north wall there is a ladder leading to a trapdoor with a one-way window.
 """
 )
