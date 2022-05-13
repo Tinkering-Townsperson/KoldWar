07d07d80c6151218e0db687dbd1fe541d9c8a9c0
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-05-12 21:54:46 -0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+import adventurelib as adv
+
+bunker = adv.Room(
+    """
+You are in the nuclear bunker. The walls are made of drab white concrete.
+Hanging on the wall are flags of the communist allies of your country, the Russian Soviet Socialist Republic.
+There is a ladder beside them
+"""
+)
