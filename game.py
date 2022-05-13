f48009d772e6cc5b4f712af9000ff3ca00cd74ab
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-05-13 00:06:30 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,16 @@
 """
-This program is licensed under the GNU Affero General Public License to AfterNoon PM and all contributors
-
+This program is licensed under the GNU Affero General Public License to AfterNoon PM and all contributors.
 """
 
 import adventurelib as adv
+import os
 
 bunker = adv.Room(
-    """
+	"""
 You are in the nuclear bunker. The walls are made of drab white concrete.
-Hanging on the wall are flags of the communist allies of your country, the Russian Soviet Socialist Republic.
-There is a ladder beside them
+Hanging on the east wall are flags of the communist states of the world, 
+including The Republic of Cuba, The Federal Republic of East Germany,
+and The People's Republic of China, forever comrades to The USSR.
+On the north wall there is a ladder leading to a trapdoor with a one-way window.
 """
 )
